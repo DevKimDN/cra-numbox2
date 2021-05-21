2d44dc9ed5f22e689bc160e715ca8553077db526
DevKimDN <kimtrong.vhit@gmail.com>
2021-05-21 22:34:21 +0700

    test first component

---
+++ src/components/NumberBox.js
@@ -0,0 +1,12 @@
+import './NumberBox.css';
+import React, { PureComponent } from 'react'
+
+export default class NumberBox extends PureComponent {
+    render() {
+        return (
+            <div className = "NumberBox">
+                <h1>Uppercase ok ! component name !!</h1>
+            </div>
+        )
+    }
+}
