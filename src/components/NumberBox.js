7fe3ba362c59d5067cf3d95ca0a62eed11ff7481
DevKimDN <kimtrong.vhit@gmail.com>
2021-05-22 16:17:02 +0700

    Finish get numX

--- src/components/NumberBox.js
+++ src/components/NumberBox.js
@@ -1,13 +1,38 @@
 import '../styles/App.css';
 import React, { PureComponent } from 'react'
 
+const axios = require('axios');
+
+
+
 export default class NumberBox extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            number : 1
+        };
+    }
+    getNumber =  async function(){      
+        try {
+          const response = await axios.get('http://server-02-devkim.herokuapp.com/60a6666fbdb08120a8071cb5');
+          const  value = response.data.data.numX.value;
+          this.setState({number : value});
+
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+
     render() {
         return (
             <div className = "NumberBox">
 
-               <input className= "submit" type="submit" value="Get from mongodb 100" />
-               <p className = "number" >100</p>
+               <input className= "submit" type="submit" value="Get from mongodb" 
+               onClick = {()=>this.getNumber()}           
+               />
+
+               <p className = "number" >{this.state.number}</p>
 
             </div>
         )
